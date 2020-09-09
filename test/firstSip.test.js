b99a0eac94ca3f02bc1a25e6f8a69fc0874c24a0
josnih <josnih@gmail.com>
2020-09-09 13:45:07 +0100

    add new tests

--- test/firstSip.test.js
+++ test/firstSip.test.js
@@ -2,6 +2,9 @@ const randomFunct = () => 0;
 
 const sumFunct = (firstNumber, secondNumber) => firstNumber + secondNumber;
 
+const codeStatementWithoutReturning = () => { 2 + 2 };
+
+const codeStatementImplicitReturn = () => 2 + 2;
 describe('First sip tests', () => {
     const testFunct = randomFunct;
     it('Functions testing', () => {
@@ -14,5 +17,24 @@ describe('First sip tests', () => {
 
     it('Sum equals', () => {
         expect(sumFunct(1, 2)).toEqual(3);
-    })
+    });
+
+    it('A block of code returning undefined', () => {
+        expect(codeStatementWithoutReturning()).toEqual(undefined);
+    });
+
+    it('One code statement return the result into arrow func', () => {
+        expect(codeStatementImplicitReturn()).toEqual(4);
+    });
+
+    it('Function evaluates inner function', () => {
+        expect((() => () => true)()()).toEqual(true);
+    });
+    it('Function argument to calculate diameter', () => {
+        expect(((diameter) => diameter * 3.14159265)(2)).toEqual(6.2831853);
+    });
+    it('Call by value behaviour', () => {
+        expect(((diameter) => diameter * 3.14159265)(1 + 1)).toEqual(6.2831853);
+    });
+
 });
